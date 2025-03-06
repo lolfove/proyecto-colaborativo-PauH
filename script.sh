1fb2496c65e58df4480a688c5c4d677fd2bd340c
Pau <Pau.correo@ejemplo.com>
2025-03-06 09:46:28 +0100

    Añade funcionalidad para saludar al usuario

--- script.sh
+++ script.sh
@@ -3,13 +3,18 @@
 echo "Menú interactivo:"
 echo "1) Mostrar archivos"
 echo "2) Mostrar fecha y hora"
-echo "3) Salir"
+echo "3) Saludar al usuario"
+echo "4) Salir"
 
 read -p "Selecciona una opción: " opcion
 
 case $opcion in
     1) ls ;;
     2) date ;;
-    3) echo "Adiós"; exit ;;
+    3) 
+        read -p "Introduce tu nombre: " nombre
+        echo "¡Hola, $nombre! Espero que disfrutes aprendiendo Git."
+        ;;
+    4) echo "Adiós"; exit ;;
     *) echo "Opción no válida";;
 esac
